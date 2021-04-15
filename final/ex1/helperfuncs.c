baf02d9f0ca7c6d15a7cd469962143961a654dfe
joaonpsilva <joao.npsilva@ua.pt>
2021-04-15 20:29:21 +0100

    helpfull functions to different file

---
+++ final/ex1/helperfuncs.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+
+int** allocTriMatrix(int oldrows, int newrows, int ** m){
+
+    m = (int**)realloc(m, newrows * sizeof(int*));
+    for(int i = oldrows; i < newrows; i++){
+        int ncols = i + 2;  //number of chars + 0 consonant case + number of words
+        m[i] = (int*)malloc(ncols * sizeof(int));
+        for (int j = 0; j < ncols; j++){
+            m[i][j] = 0;
+        }
+    }
+    return m;
+}
+
+int** prepareMatrix(int *currentRows, int row, int ** m){
+    //printf("%d %d\n", *currentRows, row);
+    row++;
+    if (row > *currentRows){
+        m = allocTriMatrix(*currentRows, row, m);
+        *currentRows = row;
+    }
+
+    return m;
+
+}
+
+int isInList(char val, char arr[], int len){
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        if(arr[i] == val)
+            return 1;
+    }
+    return 0;
+}
+
+int stopChars(char val)
+{
+    char arr[16] = {' ', '\t', '\n', '\0', '.', ',', ':', ';', '?', '!', '-', '"', '(', ')', '[',']'};
+    return isInList(val, arr, 16);
+}
+
+int isConsonant(char val)
+{
+    char arr[21] = {'b', 'c', 'd', 'f', 'g','h', 'j', 'k', 'l', 'm','n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
+    return isInList(val, arr, 21);
+}
+
+int numberOfBytesInChar(unsigned char val) {
+    if (val < 128) {
+        return 1;
+    } else if (val < 224) {
+        return 2;
+    } else if (val < 240) {
+        return 3;
+    } else {
+        return 4;
+    }
+}
