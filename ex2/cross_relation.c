6af0e9a207a652d19f4e152fd06ee6b0a13dfbb6
Tom1k <tomascosta@ua.pt>
2021-04-20 18:43:06 +0100

    FInished exercise 1 single threaded

--- ex2/cross_relation.c
+++ ex2/cross_relation.c
@@ -1,16 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** read_signal(FILE * file){
-    char buffer[1024];
-    size_t num_read = fread(buffer, sizeof(char), 1024, file);
-    if (num_read < 1024) printf("Reached EOF!\n"); 
+// auxiliary function to check if writing was done correctly
+int** check_file(FILE *file){
+    int signal_size;
+    fread(&signal_size, sizeof(int), 1, file);    
 
-    // We must explicitly NULL terminate the buffer with '\0'
-    buffer[num_read] = '\0';
+    printf("\n");
+    double x[signal_size], y[signal_size], xy[signal_size];
 
-    printf("Read %lu characters\n", num_read);
-    printf("Buffer: %s\n", buffer);
+    fread(&x, sizeof(double [signal_size]), 1, file);    
+    for(int j=signal_size-4;j<signal_size;j++) {
+         printf("WRITTEN X[%d] = %f\n", j, x[j]);
+    }
+
+    fread(&y, sizeof(double [signal_size]), 1, file);    
+    for(int j=signal_size-4;j<signal_size;j++) {
+         printf("WRITTEN Y[%d] = %f\n", j, y[j]);
+    }
+
+    fread(&xy, sizeof(double [signal_size]), 1, file);    
+    for(int j=signal_size-4;j<signal_size;j++) {
+         printf("WRITTEN XY[%d] = %f\n", j, xy[j]);
+    }
+
+    return 0;
+}
+
+int** process_signal(FILE *file){
+    // Getting the signal size
+    int signal_size;
+    fread(&signal_size, sizeof(int), 1, file);    
+    printf("Signal Size: %d\n", signal_size);
+
+    double x[signal_size], y[signal_size], xy[signal_size];
+
+    // Reading X array of doubles
+    fread(&x, sizeof(double [signal_size]), 1, file);    
+    // Reading Y array of doubles
+    fread(&y, sizeof(double [signal_size]), 1, file);    
+
+    double result = 0;
+    // Circular cross
+    for (int i=0; i<signal_size; i++){
+        for (int k=0; k<signal_size; k++){
+            result += x[k] * y[(i+k) % signal_size];
+        }
+        xy[i] = result;
+        //write to file
+        fwrite(&result, sizeof(double), 1, file);
+        result = 0;
+    }
+
+    // Append to file
+    for(int j=signal_size-4;j<signal_size;j++) {
+         printf("XY[%d] = %f\n", j, xy[j]);
+    }
+
     fclose(file);
     return 0;
 }
@@ -19,11 +65,11 @@ int main(int argc, char *argv[])
 {   
 
     for (int i = 1; i < argc; i++)
-    {     //for each arg
+    {
         
         printf("%s\n",argv[i]);
-        FILE * file;
-        file = fopen(argv[i], "rb+");     //open file
+        FILE *file;
+        file = fopen(argv[i], "ab+");     //open file
 
         if (file == NULL)
         {
@@ -32,13 +78,10 @@ int main(int argc, char *argv[])
         }
 
         // read one file, then read the other and send it to the function of processing
-        read_signal(file);
-
-    }
-
-    //after reading both files, we need to cross relate the signals
-    
-
+        process_signal(file);
+        //file = fopen(argv[i], "rb"); 
+        //check_file(file);
+    }    
     return 0;
 }
 
